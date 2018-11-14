3debe6e1ae2e7defa563f12744314308f3cb2863
Gaurav Goel <gaurav.goel167@gmail.com>
2018-11-14 13:27:29 +0530

    Added game 'Tetris' for demonstration

--- fgd.py
+++ fgd.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 from statistics import stdev
 
+
 vs = cv2.VideoCapture(0)
 
 if vs.isOpened(): # try to get the first frame
@@ -22,7 +23,8 @@ avgs_y = []
 frame_counter = 0
 gesture = ""
 
-printed = False
+mv_cnt = 0
+printed = True
 while rval:
 	bw_frame = cv2.cvtColor(new_fr, cv2.COLOR_RGB2GRAY)
 	frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
@@ -31,41 +33,27 @@ while rval:
 	fgmask2 = fgbg.apply(frame)
 	fgmask2 = cv2.morphologyEx(fgmask2, cv2.MORPH_OPEN, kernel)
 	delta = cv2.absdiff(fgmask, fgmask2)
-	# cv2.imshow("del", delta)
-	# cv2.imshow("1" , fgmask)
-	# cv2.imshow("2", fgmask2)
 	blur = cv2.GaussianBlur(delta, (21,21),0)
-
-	# cv2.imshow("masked", blur)
 	_, thresh1 = cv2.threshold(blur, 250, 255, cv2.THRESH_BINARY)
-	# _, thresh2 = cv2.threshold(fgmask, 150, 255, cv2.THRESH_BINARY)
-	# cv2.imshow("t1", thresh1)
-	# blur2 = cv2.GaussianBlur(thresh1, (11,11), 0)
 	dilated = cv2.dilate(thresh1, None, iterations=30)
 	cv2.imshow("dilate", dilated)
-	# cv2.imshow("t2", thresh2)
-	# cv2.imshow("frame", frame)
-
 
 	cnts = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	xs = []
 	ys = []
 	for c in cnts[1]:
-		# if the contour is too small or too big, ignore it
+		# if the contour is too small, ignore it
 		if cv2.contourArea(c) < 500:
 			continue
 		else:
-			# compute the bounding box for the contour, draw it on the frame,
-			# and update the text
+			# compute the bounding box for the contour
 			(x, y, w1, h1) = cv2.boundingRect(c)
 			xs.append(x + w1/2)
 			ys.append(y + h1/2)
 			cv2.rectangle(dilated, (x, y), (x + w1, y + h1), (255, 0, 0), 2)
 	if(len(xs) > 0):
 		avgs_x.append(sum(xs)/len(xs))
-		# print(avgs_x)
 		avgs_y.append(sum(ys)/len(ys))
-		# print(avgs_y)
 
 	if len(xs) == 0 and len(ys) == 0:
 		frame_counter+=1
@@ -76,31 +64,32 @@ while rval:
 		avgs_x = []
 		avgs_y = []
 		if not printed:
-			print(gesture)
+			mv_cnt += 1
+			print(str(mv_cnt) + " " + gesture)
+			moves = open("moves.txt", "a");
+			moves.write(str(mv_cnt) + " " + gesture+"\n")
+			moves.close()
 			printed = True
 
 	if(len(avgs_x) > 1 and len(avgs_y) > 1):
-		# if True:
 		if abs(avgs_x[-1] - avgs_x[-2]) <= 100 and abs(avgs_y[-1] - avgs_y[-2]) <= 100:
+			
+			## Method 1: Standard deviation based classification
 			std_x = stdev(avgs_x)
 			std_y = stdev(avgs_y)
-			# print(std_x)
-			# print(std_y)
 			if(std_y > std_x):
 				if(avgs_y[0] > avgs_y[-1]):
-					gesture = "UP"
-					# print("--UP--")
+					gesture = "ROT" #UP
 				else:
-					gesture = "DOWN"
-					# print("--DOWN--")
+					gesture = "ROT" #DOWN
 			else:
 				if(avgs_x[0] > avgs_x[-1]):
 					gesture = "LEFT"
-					# print("--LEFT--")
 				else:
 					gesture = "RIGHT"
-					# print("--RIGHT--")
 			printed = False
+			
+			## Method 2: Fitting regression line
 			# m = 0
 			# div = 0
 			# avgx = sum(avgs_x)/len(avgs_x)
